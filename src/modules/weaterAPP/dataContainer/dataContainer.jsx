2a83eab4ec3eb4a86b93e869758c7256ca350b8b
unknown <fj_rh@hotmail.com>
2023-10-07 08:12:41 -0400

    fixxed some bugs

--- src/modules/weaterAPP/dataContainer/dataContainer.jsx
+++ src/modules/weaterAPP/dataContainer/dataContainer.jsx
@@ -1,7 +1,7 @@
-import SunRise from "./SVG/sunRise.svg";
-import Sunset from "./SVG/sunSet.svg";
-import WindSpeed from "./SVG/windSpeed.svg";
-import Atmosphere from "./SVG/atmosphere.svg"
+import { ReactComponent as SunRise } from "./SVG/sunRise.svg";
+import { ReactComponent as Sunset } from "./SVG/sunSet.svg";
+import { ReactComponent as WindSpeed } from "./SVG/windSpeed.svg";
+import { ReactComponent as Atmosphere } from "./SVG/atmosphere.svg"
 
 export default function DataContainer({ sunRise, sunSet, windSpd, press }) {
 
@@ -9,24 +9,24 @@ export default function DataContainer({ sunRise, sunSet, windSpd, press }) {
         <div id="data-container">
 
             <div className="data-item cristal">
-                <img src={SunRise} alt="" className="data-item-image" />
+                <SunRise className="data-item-image" />
                 <span className="data-item-text"> Sunrise: {sunRise} UTC </span>
-            </div>       
+            </div>
 
-                <div className="data-item cristal">
-                <img src={Sunset} alt="" className="data-item-image" />
+            <div className="data-item cristal">
+                <Sunset className="data-item-image" />
                 <span className="data-item-text"> Sunset: {sunSet} UTC </span>
-            </div>  
+            </div>
 
-                <div className="data-item cristal">
-                <img src={WindSpeed} alt="" className="data-item-image" />
+            <div className="data-item cristal">
+                <WindSpeed className="data-item-image" />
                 <span className="data-item-text"> Wind Speed: {windSpd} m/s </span>
-            </div>  
+            </div>
 
-                <div className="data-item cristal">
-                <img src={Atmosphere} alt="" className="data-item-image" />
+            <div className="data-item cristal">
+                <Atmosphere className="data-item-image" />
                 <span className="data-item-text"> Pressure: {press} mb </span>
-            </div>       
+            </div>
 
 
         </div>
